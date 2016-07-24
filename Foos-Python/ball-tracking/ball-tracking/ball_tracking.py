4b0d43a8c3ca60d2b67b7af7f114ffe3d104f265
Yair Levi <yairle@microsoft.com>
2016-07-24 09:33:01 +0300

    Save the vector to file and some cleaning

--- Foos-Python/ball-tracking/ball-tracking/ball_tracking.py
+++ Foos-Python/ball-tracking/ball-tracking/ball_tracking.py
@@ -6,142 +6,146 @@
 from __future__ import print_function
 from collections import deque
 from imutils.video import WebcamVideoStream
-from imutils.video import FPS
 import numpy as np
 import argparse
 import imutils
 import cv2
+import time
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-	help="max buffer size")
-args = vars(ap.parse_args())
+__ENABLE_VIDEO_OUT__ = False
+__DEBUG_PRINT__ = False
+__PRINT_VECTOR_TO_FILE = True
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
+def getBallColor():
+	lower = (124, 91, 100)
+	upper = (179, 255, 255)
 
-# RED
-#greenLower = (170, 150, 60)
-#greenUpper = (179, 255, 255)
+	return lower,upper
 
-# BLUE
-# greenLower = (99, 161, 0)
-# greenUpper = (114, 255, 255)
+def debugPrint(txt):
+	if __DEBUG_PRINT__:
+		print(txt)
 
-# DARK BLUE
-greenLower = (39, 68, 86)
-greenUpper = (141, 161, 125)
+def savePtsVector(pts):
+	if __PRINT_VECTOR_TO_FILE:
+		txtFile = open("Output/Vector" + str(int(time.time() * 1000)) + ".txt", "w")
+		for p in pts:
+			txtFile.write("{0}	{1}\n".format(p[0],p[1]))
+		
+		txtFile.close()
 
-pts = deque(maxlen=args["buffer"])
+def main():
 
-# # if a video path was not supplied, grab the reference
-# # to the webcam
-# if not args.get("video", False):
-# 	camera = cv2.VideoCapture(1)
+	# construct the argument parse and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-v", "--video",
+		help="path to the (optional) video file")
+	ap.add_argument("-b", "--buffer", type=int, default=64,
+		help="max buffer size")
+	args = vars(ap.parse_args())
 
-# # otherwise, grab a reference to the video file
-# else:
-# 	camera = cv2.VideoCapture(args["video"])
+	# define the lower and upper boundaries of the "green"
+	# ball in the HSV color space, then initialize the
+	# list of tracked points
 
+	# BLUE
+	ballLower, ballUpper = getBallColor()
 
-# created a *threaded *video stream, allow the camera senor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from webcam...")
-vs = WebcamVideoStream(src=1).start()
-# fps = FPS().start()
+	pts = deque(maxlen=args["buffer"])
 
-
-outStream = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
-
-# keep looping
-while True:
-	# grab the current frame
-	# (grabbed, frame) = camera.read()
-
+	# created a *threaded *video stream, allow the camera senor to warmup,
+	# and start the FPS counter
+	debugPrint("[INFO] sampling THREADED frames from webcam...")
+	vs = WebcamVideoStream(src=1).start()
 	frame = vs.read()
+	if frame is None:
+		debugPrint("Changed to default camera!")
+		# Take the default camera (webcam is not connected)
+		vs = WebcamVideoStream(src=0).start()
 
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
-	# if args.get("video") and not grabbed:
-	# 	break
+	if __ENABLE_VIDEO_OUT__:
+		outStream = cv2.VideoWriter('Output/output.avi', -1, 20.0, (640,480))
 
-	# resize the frame, blur it, and convert it to the HSV
-	# color space
-	frame = imutils.resize(frame, width=640)
-	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	# keep looping
+	while True:
+		# grab the current frame
+		frame = vs.read()
 
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+		# resize the frame, blur it, and convert it to the HSV
+		# color space
+		frame = imutils.resize(frame, width=640)
+		# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
+		# construct a mask for the color "green", then perform
+		# a series of dilations and erosions to remove any small
+		# blobs left in the mask
+		mask = cv2.inRange(hsv, ballLower, ballUpper)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
 
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		# find contours in the mask and initialize the current
+		# (x, y) center of the ball
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)[-2]
+		center = None
 
-		# only proceed if the radius meets a minimum size
-		if radius > 4: #Changed from 10
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		# only proceed if at least one contour was found
+		if len(cnts) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cnts, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-	# update the points queue
-	pts.appendleft(center)
+			# only proceed if the radius meets a minimum size
+			if radius > 4: #Changed from 10
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(x), int(y)), int(radius),
+					(0, 255, 255), 2)
+				cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-	print(center)
+		# update the points queue
+		pts.appendleft(center)
 
-	# loop over the set of tracked points
-	for i in xrange(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
+		debugPrint(center)
 
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+		# loop over the set of tracked points
+		for i in xrange(1, len(pts)):
+			# if either of the tracked points are None, ignore
+			# them
+			if pts[i - 1] is None or pts[i] is None:
+				continue
 
-	# show the frame to our screen
-	cv2.imshow("Frame", frame)
-	#outStream.write(frame)
-	key = cv2.waitKey(1) & 0xFF
+			# otherwise, compute the thickness of the line and
+			# draw the connecting lines
+			thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+			cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+		# show the frame to our screen
+		cv2.imshow("Frame", frame)
+		
+		if __ENABLE_VIDEO_OUT__:
+			outStream.write(frame)
+		
+		key = cv2.waitKey(1) & 0xFF
 
-	# update the FPS counter
-	# fps.update()
+		# if the 'q' key is pressed, stop the loop
+		if key == ord("q"):
+			break
 
-# stop the timer and display FPS information
-# fps.stop()
-# print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-# print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	# cleanup the camera and close any open windows
+	cv2.destroyAllWindows()
+	vs.stop()
+	savePtsVector(pts)
+	raise SystemExit
+	if __ENABLE_VIDEO_OUT__:
+		outStream.release()
 
 
-# cleanup the camera and close any open windows
-cv2.destroyAllWindows()
-vs.stop()
-outStream.release()
+
+if __name__ == "__main__":
+    main()
