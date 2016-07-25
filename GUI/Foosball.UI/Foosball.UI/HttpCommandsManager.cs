8b9fdfd7fc16971231d00502114ee1d479b5fd44
Moshe Cohen <moshec@microsoft.com>
2016-07-25 12:19:38 +0300

    UI

---
+++ GUI/Foosball.UI/Foosball.UI/HttpCommandsManager.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Foosball.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    public class HttpCommandsManager : IDisposable
+    {
+        private readonly Dictionary<string, HttpEvent> onGoalEvent;
+        private readonly HttpListener listener;
+
+        private readonly List<string> prefixes = new List<string> { "http://localhost/foosballApi/" };
+
+        public HttpCommandsManager(Dictionary<string, HttpEvent> onGoalEvent)
+        {
+            this.onGoalEvent = onGoalEvent;
+            this.listener = new HttpListener();
+            foreach (string prefix in this.prefixes)
+            {
+                this.listener.Prefixes.Add(prefix);
+            }
+
+            this.listener.Start();
+            Console.WriteLine("Listening...");
+
+            Task.Run((Action) Listen);
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                while (this.listener.IsListening)
+                {
+                    // Blocking command
+                    HttpListenerContext context = this.listener.GetContext();
+                    try
+                    {
+                        string[] payload = new StreamReader(context.Request.InputStream).ReadToEnd().Split('*');
+                        Team team = (Team) Enum.Parse(typeof (Team), payload[1], true);
+
+                        // Handle command asynchronously
+                        Task.Run(() => this.onGoalEvent[payload[0]](team));
+
+                        // Return OK response
+                        context.Response.StatusCode = 200;
+                        using (StreamWriter outputStream = new StreamWriter(context.Response.OutputStream))
+                        {
+                            outputStream.Write("OK");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        // Return error response
+                        context.Response.StatusCode = 500;
+                        using (StreamWriter outputStream = new StreamWriter(context.Response.OutputStream))
+                        {
+                            outputStream.Write(e.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error occurred: {0}", exception);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.listener.Stop();
+            this.listener.Close();
+        }
+    }
+}
---
+++ GUI/Foosball.UI/Foosball.UI/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Foosball.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.IO;
+    using System.Reflection;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+
+    public enum Team
+    {
+        Red,
+        Blue
+    }
+
+    public delegate void HttpEvent(Team team);
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly HttpCommandsManager httpManager;
+
+        // Define event
+        public event HttpEvent GoalEvent;
+        public event HttpEvent MissEvent;
+        public event HttpEvent StartEvent;
+        public event HttpEvent StatusEvent;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Assign a function
+            GoalEvent += OnGoalEvent;
+            MissEvent += OnMissEvent;
+            StatusEvent += OnStatusEvent;
+            StartEvent += OnStartEvent;
+            
+            // Send the event to the HTTP manager so it can be invoked by need
+            this.httpManager = new HttpCommandsManager(new Dictionary<string, HttpEvent>
+            {
+                {"Status", StatusEvent},
+                {"Goal", GoalEvent},
+                {"Miss", MissEvent},
+                {"Start", StartEvent},
+            });
+        }
+
+        private void OnStartEvent(Team team)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(Path.Combine(Directory.GetCurrentDirectory(), "Open.mp3")));
+            mediaPlayer.Play();
+        }
+
+        private void OnStatusEvent(Team team)
+        {
+            // UI commands must be invoked from this command since this is the BG thread and not the UI thread
+            Dispatcher.Invoke(ChangeBgColor);
+        }
+
+        private void OnMissEvent(Team team)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                this.DockPanel.Children.Clear();
+                this.DockPanel.Children.Add(new MediaElement { Source = new Uri("C:/Users/moshec/Desktop/SoccerBallMiss.gif") });
+            });
+        }
+
+        private void OnGoalEvent(Team team)
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(Path.Combine(Directory.GetCurrentDirectory(), team == Team.Blue ? @"Goal-Brazil.mp3" : @"Goal-Meir.mp3")));
+            mediaPlayer.Play();
+        }
+
+        private void ChangeBgColor()
+        {
+            Background = PickBrush();
+        }
+
+        private Brush PickBrush()
+        {
+            Brush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            int random = rnd.Next(properties.Length);
+            result = (Brush)properties[random].GetValue(null, null);
+
+            return result;
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            this.httpManager.Dispose();
+        }
+    }
+}
