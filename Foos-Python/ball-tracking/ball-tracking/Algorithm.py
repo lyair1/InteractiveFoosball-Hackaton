d98ca8188274247eaccf3442792f5b2a66f8bf37
Yair Levi <yairle@microsoft.com>
2016-07-25 17:08:02 +0300

    moving the algorithm code

--- Foos-Python/ball-tracking/ball-tracking/Algorithm.py
+++ Foos-Python/ball-tracking/ball-tracking/Algorithm.py
@@ -18,84 +18,84 @@ class Algorithm:
 	Width = 3
 	NoneCountTH = 8
 	possesionMatrix = []
-        noneCount = 0
+	noneCount = 0
 
-        def inArea(self, point, lowX, highX, lowY, highY):
-            if (point[0]>lowX and point[0]<highX and point[1]>lowY and point[1]<highY):
-                return True
-            else:
-                return False         
-        
-        def inCenter(self, point):
-            return self.inArea(point, 100, 50, 100, 50)
-                          
-        def inRedDangerZone(self, point):
-            return self.inArea(point, 120, 60, 120, 60)
-            
-        def inBlueDangerZone(self, point):
-            return self.inArea(point, o, 20, 0, 20)
-        
-        def HandleOOF(self, point):
-            if (self.inCenter(point)):
-                self.state = STATE.IN_PLAY
-                sendEvent(EVENT.START, 1)
-            debugPrint("End HandleOOF")
-            
-        def HandleDangerZone(self, point):
-            if (noneCount == NoneCountTH):
-                if (self.state == STATE.DANGER_ZONE_RED):
-                    sendEvent(EVENT.GOAL, 'BLUE')
-                else:
-                    sendEvent(EVENT.GOAL, 'RED')
-                self.state = STATE.OOF
-            if not(self.inDangerZoneRed(point) or self.inDangerZoneBlue(point)):
-                self.state = STATE.IN_PLAY
-            debugPrint("End HandleDangerZone")
+	def inArea(self, point, lowX, highX, lowY, highY):
+		if (point[0]>lowX and point[0]<highX and point[1]>lowY and point[1]<highY):
+			return True
+		else:
+			return False         
+	
+	def inCenter(self, point):
+		return self.inArea(point, 0, 1000, 800, 1200)
+					  
+	def inRedDangerZone(self, point):
+		return self.inArea(point, 200, 800, 0, 250)
+		
+	def inBlueDangerZone(self, point):
+		return self.inArea(point, 200, 800, 1750, 2000)
+	
+	def HandleOOF(self, point):
+		if (self.inCenter(point)):
+			self.state = STATE.IN_PLAY
+			sendEvent(EVENT.START, 1)
+		debugPrint("End HandleOOF")
+		
+	def HandleDangerZone(self, point):
+		if (noneCount == NoneCountTH):
+			if (self.state == STATE.DANGER_ZONE_RED):
+				sendEvent(EVENT.GOAL, 'BLUE')
+			else:
+				sendEvent(EVENT.GOAL, 'RED')
+			self.state = STATE.OOF
+		if not(self.inDangerZoneRed(point) or self.inDangerZoneBlue(point)):
+			self.state = STATE.IN_PLAY
+		debugPrint("End HandleDangerZone")
 
-        def HandleInPlay(self, point):
-            if (self.inDangerZoneRed(point)):
-                self.state = STATE.DANGER_ZONE_RED
-                redDangerZoneCount+=1
-            if (self.inDangerZoneBlue(point)):
-                self.state = STATE.DANGER_ZONE_BLUE
-                blueDangerZoneCount+=1
-            debugPrint("End HandleDangerZone")	
+	def HandleInPlay(self, point):
+		if (self.inDangerZoneRed(point)):
+			self.state = STATE.DANGER_ZONE_RED
+			redDangerZoneCount+=1
+		if (self.inDangerZoneBlue(point)):
+			self.state = STATE.DANGER_ZONE_BLUE
+			blueDangerZoneCount+=1
+		debugPrint("End HandleDangerZone")	
 
 	def __init__(self):
-            	self.state = STATE.OOF
+		self.state = STATE.OOF
 		redDangerZoneCount = 0
 		blueDangerZoneCount = 0
 		for i in range(self.Width):
 			self.possesionMatrix.append([])
 			for j in range(self.Length):
-				self.possesionMatrix[i].append(0)             
+				self.possesionMatrix[i].append(0)
+
+		self.debugFile = open("debug", "w")
 		debugPrint("End main")
 		
 	def AddPoints(self, pointsArray):
-                for point in pointsArray :
+		for point in pointsArray :
+			lastState = self.state
 
-                    ########## Update debugging matrix ##########################
-                    self.possesionMatrix[point[0]][point[1]] +=1
-                    debugPrint("AddPoints")
-                    debugPrint(self.possesionMatrix)
-                    debugPrint("Point: " + str(point[0]) + "," + str(point[1]))
-                    #############################################################
+			#############################################################
 
-                    if (point[0] == -1 and point[1] == -1):
-                        noneCount+=1
-                    else:
-                        noneCount = 0
+			if (point[0] == -1 and point[1] == -1):
+				noneCount+=1
+			else:
+				noneCount = 0
 
-                    ###### Call the relevant handler according the state ##############################
-                    if (self.state == STATE.IN_PLAY):
-                        self.HandleInPlay(point)
-                    if (self.state == STATE.DANGER_ZONE_RED or self.state == STATE.DANGER_ZONE_BLUE):
-                        self.HandleDangerZone(point)
-                    if (self.state == STATE.OOF):
-                        self.HandleOOF(point)
-                    ####################################################################################
+			###### Call the relevant handler according the state ##############################
+			if (self.state == STATE.IN_PLAY):
+				self.HandleInPlay(point)
+			if (self.state == STATE.DANGER_ZONE_RED or self.state == STATE.DANGER_ZONE_BLUE):
+				self.HandleDangerZone(point)
+			if (self.state == STATE.OOF):
+				self.HandleOOF(point)
 
-                    debugPrint("state is: " + str(self.state))
+			self.debugFile.write("Point: " + str(point[0]) + "," + str(point[1]))
+			debugPrint("Point: " + str(point[0]) + "," + str(point[1]))
+			self.debugFile.write("state: " + str(self.state))
+			debugPrint("state: " + str(self.state))
 
 
 class GuiHttpClient(object):
@@ -119,7 +119,7 @@ class EventHook(object):
 
 def main():
 	x = EventHook()
-	x.fire([[1,2]])
+	x.fire([[1,2], [999, 1999]])
 	
 
 if __name__ == "__main__":
